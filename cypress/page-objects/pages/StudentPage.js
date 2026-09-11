decb3b4fae500a8be6f390070bdc8072dd1d2b4f
birendermail-beep <birender.mail@gmail.com>
2026-09-11 23:38:29 +0530

    Use current Jigyaasa Platform Demo course for student tests

--- cypress/page-objects/pages/StudentPage.js
+++ cypress/page-objects/pages/StudentPage.js
@@ -13,8 +13,8 @@ export default class StudentPage extends BasePage {
     }
 
     static openStudentDashboard() {
-        const courseCrn = Cypress.env('STUDENT_COURSE_CRN') || 'LO-Aplus-complete'
-        const searchText = Cypress.env('STUDENT_COURSE_SEARCH') || 'lo-a'
+        const courseCrn = Cypress.env('STUDENT_COURSE_CRN') || 'Demo.AA1'
+        const searchText = Cypress.env('STUDENT_COURSE_SEARCH') || 'Platform Demo'
         this.searchAndManageCourse(searchText, courseCrn)
     }
 
@@ -25,7 +25,7 @@ export default class StudentPage extends BasePage {
 
     static openurl() {
         const courseCrn = Cypress.env('DEMO_COURSE_CRN') || 'Demo.AA1'
-        const searchText = Cypress.env('DEMO_COURSE_SEARCH') || 'platform Demo'
+        const searchText = Cypress.env('DEMO_COURSE_SEARCH') || 'Platform Demo'
         this.searchAndManageCourse(searchText, courseCrn)
         LoginPage.visitOnClick('.span13 > .btn-outline-primary')
     }
@@ -108,12 +108,12 @@ export default class StudentPage extends BasePage {
         cy.visit(`${targetUrl}/?func=load_course&course_code=${targetCourseCode}&class_code=${targetClassCode}`)
     }
 
-    static visitLOAplusCompleteCourse(data) {
-        const targetUrl = Cypress.config('baseUrl') || data.url
-        const course = Cypress.env('STUDENT_COURSE_CRN') || 'LO-Aplus-complete'
-        const classCode = Cypress.env('STUDENT_CLASS_CODE') || data.class_code[10]
+    static visitLOAplusCompleteCourse() {
+        const course = Cypress.env('STUDENT_COURSE_CRN') || 'Demo.AA1'
+        const deskCopy = Cypress.env('STUDENT_DESK_COPY') || '1'
 
-        cy.visit(`${targetUrl}/?func=load_course&course=${course}&class_code=${classCode}`)
+        cy.visit(`/?func=load_course&course=${encodeURIComponent(course)}&desk_copy=${encodeURIComponent(deskCopy)}`)
+        cy.contains('POST ASSESSMENT', { timeout: 30000 }).should('be.visible')
     }
 
     static loadCourse() {
