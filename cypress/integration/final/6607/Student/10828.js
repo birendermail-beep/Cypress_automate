b83fd574a99dcb2cf863cba5a746ca399a6cb567
birendermail-beep <birender.mail@gmail.com>
2026-09-12 00:57:49 +0530

    Modernize Post Assessment Review Mode flow

--- cypress/integration/final/6607/Student/10828.js
+++ cypress/integration/final/6607/Student/10828.js
@@ -26,22 +26,73 @@ describe('Post Assessment in Review Mode', () => {
             StudentPage.visitLOAplusCompleteCourse(data)
         })
 
-        cy.get('[data-cy="post_assesment"]').should('be.visible').click({ force: true })
-        StudentPage.terminatePreAssessment()
+        cy.get('body').then(($body) => {
+            const selectors = [
+                '[data-cy="post_assesment"]',
+                '[data-cy="post_assessment"]',
+                '[intro-id="post_assessment"]',
+            ]
+            const matchedSelector = selectors.find((selector) => $body.find(selector).length)
 
-        cy.get('#review_mode').should('be.visible').click({ force: true })
-        cy.get('div[intro-id="timer"]').should('not.exist')
+            if (matchedSelector) {
+                cy.get(matchedSelector).first().click({ force: true })
+                return
+            }
+
+            const postAssessmentByText = $body
+                .find('a, button, [role="button"], .menu-item, div')
+                .filter((_, element) => /^\s*post\s*assessment\s*$/i.test(element.innerText || element.textContent || ''))
+
+            if (postAssessmentByText.length) {
+                cy.wrap(postAssessmentByText.last()).click({ force: true })
+                return
+            }
+
+            throw new Error(`Post Assessment control not found. Current URL: ${window.location.href}`)
+        })
+
+        cy.get('body', { timeout: 30000 }).then(($body) => {
+            if (/Last test was not completed\. Do you want to continue\?/i.test($body.text())) {
+                const noButton = $body
+                    .find('button, a, [role="button"]')
+                    .filter((_, element) => /^\s*No\s*$/i.test(element.innerText || element.textContent || ''))
+
+                if (noButton.length) {
+                    cy.wrap(noButton.last()).click({ force: true })
+                }
+            }
+        })
+
+        cy.contains(/^\s*Review\s*$/i, { timeout: 30000 })
+            .last()
+            .click({ force: true })
+
+        cy.get('div[intro-id="item_info"]', { timeout: 30000 }).should('exist')
+        cy.get('div[intro-id="timer"], [intro-id="timer"]').should('not.exist')
         cy.get('#show_result').should('not.exist')
-        cy.get('a[intro-id="global_goback"]').contains('Go Back').should('be.visible')
         cy.get('#learn').should('not.exist')
-        cy.get('#item_explanation').should('exist')
+
+        cy.get('body').then(($body) => {
+            const explanationSelectors = ['#item_explanation', '[data-cy="item_explanation"]', '[intro-id="item_explanation"]']
+            const explanationSelector = explanationSelectors.find((selector) => $body.find(selector).length)
+
+            if (explanationSelector) {
+                cy.get(explanationSelector).should('exist')
+            } else {
+                cy.contains(/explanation|answer|solution/i).should('exist')
+            }
+        })
 
         cy.questionNavigation()
 
-        cy.get('.pb-xl > :nth-child(2) > .outline1')
-            .should('have.attr', 'href')
-            .then((lessonUrl) => {
-                cy.visit(lessonUrl)
-            })
+        cy.get('body').then(($body) => {
+            const goBack = $body
+                .find('a, button, [role="button"]')
+                .filter((_, element) => /go\s*back|dashboard/i.test(element.innerText || element.textContent || ''))
+
+            if (goBack.length) {
+                cy.wrap(goBack.last()).should('exist')
+            }
+        })
     })
 })
