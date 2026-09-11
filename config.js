76fe956386c8fee5040395b2b9abeb69771fa6d2
birendermail-beep <birender.mail@gmail.com>
2026-09-11 22:34:07 +0530

    Remove hardcoded login credentials

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-const login_username = "birender.singh@ucertify.com"
-const login_password = "uCertify@20261 "
+const login_username = process.env.CYPRESS_USERNAME || ''
+const login_password = process.env.CYPRESS_PASSWORD || ''
+
 module.exports = { login_username, login_password }
