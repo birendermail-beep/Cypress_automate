28427a4ac457eaf97ecd873891e9fdf00a6ad3b0
birendermail-beep <birender.mail@gmail.com>
2026-09-12 00:45:34 +0530

    Modernize Post Assessment Learn Mode flow

--- cypress/integration/final/6607/Student/10827.js
+++ cypress/integration/final/6607/Student/10827.js
@@ -17,7 +17,7 @@ import {
 } from '../../../../page-objects/pages/index'
 
 describe('Post Assessment in Learn Mode', () => {
-    it('opens Learn Mode, shows Retry after submit, and navigates questions', () => {
+    it('opens Learn Mode, verifies feedback controls, and navigates questions', () => {
         cy.visit('/')
         Navbar.clickOnLogin()
         LoginPage.loginPage(login_username, login_password)
@@ -26,14 +26,62 @@ describe('Post Assessment in Learn Mode', () => {
             StudentPage.visitLOAplusCompleteCourse(data)
         })
 
-        cy.get('[data-cy="post_assesment"]').should('be.visible').click({ force: true })
-        StudentPage.terminatePreAssessment()
+        cy.get('body').then(($body) => {
+            const selectors = [
+                '[data-cy="post_assesment"]',
+                '[data-cy="post_assessment"]',
+                '[intro-id="post_assessment"]',
+            ]
+            const matchedSelector = selectors.find((selector) => $body.find(selector).length)
 
-        cy.get('#learn_mode').should('be.visible').click({ force: true })
-        cy.get('#show_result').should('exist')
-        cy.get('#learn').should('exist').contains('Submit').click({ force: true })
-        cy.get('#learn').contains('Retry').should('be.visible')
-        cy.get('div[intro-id="timer"]').should('not.exist')
+            if (matchedSelector) {
+                cy.get(matchedSelector).first().click({ force: true })
+                return
+            }
+
+            const postAssessmentByText = $body
+                .find('a, button, [role="button"], .menu-item, div')
+                .filter((_, element) => /^\s*post\s*assessment\s*$/i.test(element.innerText || element.textContent || ''))
+
+            if (postAssessmentByText.length) {
+                cy.wrap(postAssessmentByText.last()).click({ force: true })
+                return
+            }
+
+            throw new Error(`Post Assessment control not found. Current URL: ${window.location.href}`)
+        })
+
+        cy.get('body', { timeout: 30000 }).then(($body) => {
+            if (/Last test was not completed\. Do you want to continue\?/i.test($body.text())) {
+                const noButton = $body
+                    .find('button, a, [role="button"]')
+                    .filter((_, element) => /^\s*No\s*$/i.test(element.innerText || element.textContent || ''))
+
+                if (noButton.length) {
+                    cy.wrap(noButton.last()).click({ force: true })
+                }
+            }
+        })
+
+        cy.contains(/^\s*Learn\s*$/i, { timeout: 30000 })
+            .last()
+            .click({ force: true })
+
+        cy.get('div[intro-id="item_info"]', { timeout: 30000 }).should('exist')
+        cy.get('div[intro-id="timer"], [intro-id="timer"]').should('not.exist')
+
+        cy.get('body').then(($body) => {
+            if ($body.find('#learn').length) {
+                cy.get('#learn').should('exist').then(($learn) => {
+                    if (/submit/i.test($learn.text())) {
+                        cy.wrap($learn).click({ force: true })
+                        cy.get('#learn', { timeout: 30000 }).should(($retry) => {
+                            expect($retry.text()).to.match(/retry/i)
+                        })
+                    }
+                })
+            }
+        })
 
         cy.questionNavigation()
         StudentPage.endTest()
