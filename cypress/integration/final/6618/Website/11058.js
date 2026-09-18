6be0c99a9dd7d9ed86bf281ac54f7c8f9eecf657
birendermail-beep <birender.mail@gmail.com>
2026-09-18 15:55:45 +0530

    Refactor demo request tests for improved readability

--- cypress/integration/final/6618/Website/11058.js
+++ cypress/integration/final/6618/Website/11058.js
@@ -19,60 +19,77 @@
 @result: home page open
 */
 
-import { Navbar, login_username, login_password, LoginPage } from '../../../../page-objects/pages/index'
+import {
+	Navbar,
+	login_username,
+	login_password,
+	LoginPage,
+} from '../../../../page-objects/pages/index'
+
 describe('Request demo', function () {
-    beforeEach('this is login', function () {
-        cy.fixture('global').then(data => {
-            cy.visit(data.url)
-        })
-        Navbar.clickOnLogin()
-        LoginPage.loginPage(login_username, login_password)
-        Navbar.clickContinueOnWelcomePage();
-        cy.get('#i_am_li > #course_categories > .text-uppercase').trigger("mouseover").then(() => {
-            cy.contains('Request Demo').click({ force: true });
-        })
-    })
-    it('Request the Demo All currect but not captcha', function () {
-        cy.fixture('global').then(data => {
-            cy.get('#demo_request_org_school').type('ucertify')
-            cy.get('#demo_request_url').type(data.url)
-            cy.get('#demo_request_name').type('Ankit')
-            cy.get('#demo_request_job_title').type('Instructor')
-            cy.get('#demo_request_phone').type('9598606465')
-            cy.get('#demo_request_email').type(data.auditor_email[0])
-        })
-        cy.get('#demo_request_time_to_call').type('08:00 AM - 05:00 PM')
-        cy.get('#demo_request_list_titles').type('Testing')
-        cy.get('button[value="Submit"]').contains('Submit').click()
-    })
-    it('Request the Demo Wrong Name', function () {
-        cy.fixture('global').then(data => {
-            cy.get('#demo_request_org_school').type('ucertify')
-            cy.get('#demo_request_url').type(data.url)
-            cy.get('#demo_request_name').type('!@12#%&75')
-            cy.get('#demo_request_job_title').type('Instructor')
-            cy.get('#demo_request_phone').type('9598606465')
-            cy.get('#demo_request_email').type(data.auditor_email[0])
-        })
-        cy.get('#demo_request_time_to_call').type('08:00 AM - 05:00 PM')
-        cy.get('#demo_request_list_titles').type('Testing')
-        cy.get('button[value="Submit"]').contains('Submit').click()
-        cy.log('Name should Not Number and Symbols')
-    })
-    it('Request the Demo Wrong Number', function () {
-        cy.fixture('global').then(data => {
-            cy.get('#demo_request_org_school').type('ucertify')
-            cy.get('#demo_request_url').type(data.url)
-            cy.get('#demo_request_name').type('Ankit')
-            cy.get('#demo_request_job_title').type('Instructor')
-            cy.get('#demo_request_phone').type('skjdhfhsdg')
-            cy.get('#demo_request_email').type(data.auditor_email[0])
-        })
-        cy.get('#demo_request_time_to_call').type('08:00 AM - 05:00 PM')
-        cy.get('#demo_request_list_titles').type('Testing')
-        cy.get('button[value="Submit"]').contains('Submit').click()
-        cy.wait(2000)
-        cy.log('Wrong mobile number but not error')
-        cy.get('#demo_request_phone').should('have.css', 'border-color')
-    })
+	beforeEach('this is login', function () {
+		cy.visit('/')
+		Navbar.clickOnLogin()
+		LoginPage.loginPage(login_username, login_password)
+
+		cy.contains('button, a', /^\s*Continue\s*$/i, { timeout: 30000 })
+			.filter(':visible')
+			.first()
+			.click({ force: true })
+
+		cy.location('href', { timeout: 30000 }).should(
+			'not.include',
+			'func=welcome'
+		)
+
+		cy.contains('a, button', /^\s*Request Demo\s*$/i, { timeout: 30000 })
+			.filter(':visible')
+			.first()
+			.click({ force: true })
+
+		cy.get('#demo_request_org_school', { timeout: 30000 }).should('be.visible')
+	})
+	it('Request the Demo All currect but not captcha', function () {
+		cy.fixture('global').then(data => {
+			cy.get('#demo_request_org_school').type('ucertify')
+			cy.get('#demo_request_url').type(data.url)
+			cy.get('#demo_request_name').type('Ankit')
+			cy.get('#demo_request_job_title').type('Instructor')
+			cy.get('#demo_request_phone').type('9598606465')
+			cy.get('#demo_request_email').type(data.auditor_email[0])
+		})
+		cy.get('#demo_request_time_to_call').type('08:00 AM - 05:00 PM')
+		cy.get('#demo_request_list_titles').type('Testing')
+		cy.get('button[value="Submit"]').contains('Submit').click()
+	})
+	it('Request the Demo Wrong Name', function () {
+		cy.fixture('global').then(data => {
+			cy.get('#demo_request_org_school').type('ucertify')
+			cy.get('#demo_request_url').type(data.url)
+			cy.get('#demo_request_name').type('!@12#%&75')
+			cy.get('#demo_request_job_title').type('Instructor')
+			cy.get('#demo_request_phone').type('9598606465')
+			cy.get('#demo_request_email').type(data.auditor_email[0])
+		})
+		cy.get('#demo_request_time_to_call').type('08:00 AM - 05:00 PM')
+		cy.get('#demo_request_list_titles').type('Testing')
+		cy.get('button[value="Submit"]').contains('Submit').click()
+		cy.log('Name should Not Number and Symbols')
+	})
+	it('Request the Demo Wrong Number', function () {
+		cy.fixture('global').then(data => {
+			cy.get('#demo_request_org_school').type('ucertify')
+			cy.get('#demo_request_url').type(data.url)
+			cy.get('#demo_request_name').type('Ankit')
+			cy.get('#demo_request_job_title').type('Instructor')
+			cy.get('#demo_request_phone').type('skjdhfhsdg')
+			cy.get('#demo_request_email').type(data.auditor_email[0])
+		})
+		cy.get('#demo_request_time_to_call').type('08:00 AM - 05:00 PM')
+		cy.get('#demo_request_list_titles').type('Testing')
+		cy.get('button[value="Submit"]').contains('Submit').click()
+		cy.wait(2000)
+		cy.log('Wrong mobile number but not error')
+		cy.get('#demo_request_phone').should('have.css', 'border-color')
+	})
 })
