ae102810fd855ffedffec9380e91106ce5b4be93
birendermail-beep <birender.mail@gmail.com>
2026-09-24 20:47:23 +0530

    Modernize course dashboard student spec

--- cypress/integration/final/6607/Student/11024.js
+++ cypress/integration/final/6607/Student/11024.js
@@ -1,35 +1,12 @@
-/*
-@author: Anirudha Pratap
-@master_project_id: 6607
-@phase_id: 9327
-@story_id: 11024
-@story_name: Course Dashboard
-@path: final/Student/6607
-@test_case_name: Course Dashboard
-@description: N/A
-@test_steps:
+/* @story_id: 11024 @story_name: Course Dashboard */
+import { visitDemoCourse } from '../../../../support/student-auth'
 
-^search on my library 
--Open https://ucertify.com/ 
--Login with your account
--Click on My Library and search ICT course as -test  (you can open any one)
-
-@test_data:N/A
-@result: search on my library 
-*/
-
-import { Navbar, login_username, login_password, LoginPage, StudentPage } from '../../../../page-objects/pages/index' 
-describe('ebook area testing', function() {
-    //ict.dashboard1
-    it('ICT courses dahboard', function() {
-        cy.fixture('global').then(data => {
-            cy.visit(data.url)
-        })
-        Navbar.clickOnLogin()
-        LoginPage.loginPage(login_username, login_password)
-    cy.get('[data-cy="mylibrary"]').click({ force: true })
-    cy.get('[data-cy="searchbox"]').type('ict')
-    cy.get('[crn="ICT-spreadsheet-test"]').contains('Manage').click({ force: true })
-    cy.contains('desk copy').click({ force: true })
+describe('Course Dashboard', () => {
+    it('opens the Demo.AA1 course dashboard', () => {
+        visitDemoCourse()
+        cy.get('body', { timeout: 30000 })
+            .should('be.visible')
+            .and('contain.text', 'Course Dashboard')
+            .and('not.contain.text', 'Default blank page')
     })
 })
