d9a44c56f4b24799a5abcd0d7919528565f4e4a0
birendermail-beep <birender.mail@gmail.com>
2026-09-24 16:32:57 +0530

    Target exact lesson toolbar control in 10941.js

--- cypress/integration/final/6607/Student/10941.js
+++ cypress/integration/final/6607/Student/10941.js
@@ -1,14 +1,15 @@
 /* @story_id: 10941 @story_name: Open lab */
-import {
-    clickLessonNextStep,
-    openDemoLesson,
-} from '../../../../support/student-auth'
+import { openDemoLesson } from '../../../../support/student-auth'
 
 describe('Lesson next steps - Lab', () => {
     it('opens the chapter lab without a blank page', () => {
         openDemoLesson()
         cy.scrollTo('bottom')
-        clickLessonNextStep(/Hands-on lab|Hands on lab|Gain experience.*lab|Lab/i)
+        cy.contains('button, a, [role="button"]', /^\s*Labs?\s*$/i, {
+            timeout: 30000,
+        }).filter(':visible').last()
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
         cy.get('body', { timeout: 30000 })
             .should('be.visible')
             .and('not.contain.text', 'Default blank page')
