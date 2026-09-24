f94a998f568e8cbc48a28d0b31871bb9f7d92688
birendermail-beep <birender.mail@gmail.com>
2026-09-24 16:01:36 +0530

    Use visible lesson next-step actions in 10942.js

--- cypress/integration/final/6607/Student/10942.js
+++ cypress/integration/final/6607/Student/10942.js
@@ -1,33 +1,15 @@
 /* @story_id: 10942 @story_name: Go to next chapter */
-import { openDemoLesson } from '../../../../support/student-auth'
+import {
+    clickLessonNextStep,
+    openDemoLesson,
+} from '../../../../support/student-auth'
 
 describe('Lesson next steps - Next lesson', () => {
     it('continues to the next lesson', () => {
         openDemoLesson()
         cy.scrollTo('bottom')
         cy.location('href').then(startUrl => {
-            cy.contains(':visible', /Proceed to the next lesson|Next lesson/i, {
-                timeout: 30000,
-            }).first().then($label => {
-                const directControl = $label.closest('a, button, [role="button"]')
-                if (directControl.length) {
-                    cy.wrap(directControl.first()).click({ force: true })
-                    return
-                }
-
-                let container = $label.parent()
-                let control = Cypress.$()
-                for (let depth = 0; depth < 6 && container.length; depth += 1) {
-                    control = container.find('a, button, [role="button"]')
-                        .filter(':visible')
-                        .filter((_, element) => /Open|Next|Continue/i.test(element.textContent))
-                    if (control.length) break
-                    container = container.parent()
-                }
-
-                expect(control.length, 'Next lesson action').to.be.greaterThan(0)
-                cy.wrap(control.first()).click({ force: true })
-            })
+            clickLessonNextStep(/Proceed to the next lesson|Next lesson/i)
             cy.location('href', { timeout: 30000 }).should('not.eq', startUrl)
         })
         cy.get('body').should('not.contain.text', 'Default blank page')
