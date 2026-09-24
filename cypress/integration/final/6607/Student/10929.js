a5acd283d28feb2916dececdcf6b9ecf06b54cce
birendermail-beep <birender.mail@gmail.com>
2026-09-24 15:06:45 +0530

    Use current lesson settings selector in 10929.js

--- cypress/integration/final/6607/Student/10929.js
+++ cypress/integration/final/6607/Student/10929.js
@@ -4,7 +4,9 @@ import { openDemoLesson } from '../../../../support/student-auth'
 describe('Manage settings options in the lesson area', () => {
     it('shows the lesson settings options', () => {
         openDemoLesson()
-        cy.get('#manage_settg', { timeout: 30000 })
+        cy.get('#manage_settg, .icomoon-new-24px-gear-1, [aria-label*="Settings" i]', {
+            timeout: 30000,
+        })
             .filter(':visible').first().click({ force: true })
         cy.get('#fcs', { timeout: 30000 }).should('be.visible')
         cy.get('body').should($body => {
