893a3ba9d3c68d73dc71a1a029a3083359f4b77b
birendermail-beep <birender.mail@gmail.com>
2026-09-24 16:37:39 +0530

    Handle lesson activity popup in 10941.js

--- cypress/integration/final/6607/Student/10941.js
+++ cypress/integration/final/6607/Student/10941.js
@@ -1,15 +1,14 @@
 /* @story_id: 10941 @story_name: Open lab */
-import { openDemoLesson } from '../../../../support/student-auth'
+import {
+    openDemoLesson,
+    openLessonToolbarActivity,
+} from '../../../../support/student-auth'
 
 describe('Lesson next steps - Lab', () => {
     it('opens the chapter lab without a blank page', () => {
         openDemoLesson()
         cy.scrollTo('bottom')
-        cy.contains('button, a, [role="button"]', /^\s*Labs?\s*$/i, {
-            timeout: 30000,
-        }).filter(':visible').last()
-            .invoke('removeAttr', 'target')
-            .click({ force: true })
+        openLessonToolbarActivity(/^\s*Labs?\s*$/i)
         cy.get('body', { timeout: 30000 })
             .should('be.visible')
             .and('not.contain.text', 'Default blank page')
