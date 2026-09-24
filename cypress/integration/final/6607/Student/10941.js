04d6a18d2bf967352f9555705a2583fe57d3bc07
birendermail-beep <birender.mail@gmail.com>
2026-09-24 16:01:24 +0530

    Use visible lesson next-step actions in 10941.js

--- cypress/integration/final/6607/Student/10941.js
+++ cypress/integration/final/6607/Student/10941.js
@@ -1,13 +1,14 @@
 /* @story_id: 10941 @story_name: Open lab */
-import { openDemoLesson } from '../../../../support/student-auth'
+import {
+    clickLessonNextStep,
+    openDemoLesson,
+} from '../../../../support/student-auth'
 
 describe('Lesson next steps - Lab', () => {
     it('opens the chapter lab without a blank page', () => {
         openDemoLesson()
         cy.scrollTo('bottom')
-        cy.get('[data-cy="lab_open"]', { timeout: 30000 })
-            .filter(':visible').first().scrollIntoView()
-            .invoke('removeAttr', 'target').click({ force: true })
+        clickLessonNextStep(/Hands-on lab|Hands on lab|Gain experience.*lab|Lab/i)
         cy.get('body', { timeout: 30000 })
             .should('be.visible')
             .and('not.contain.text', 'Default blank page')
