ee2d4bfa9294da67dcb42142a6fcf537c4b0b6fe
birendermail-beep <birender.mail@gmail.com>
2026-09-24 16:33:10 +0530

    Target exact lesson toolbar control in 10942.js

--- cypress/integration/final/6607/Student/10942.js
+++ cypress/integration/final/6607/Student/10942.js
@@ -1,15 +1,14 @@
 /* @story_id: 10942 @story_name: Go to next chapter */
-import {
-    clickLessonNextStep,
-    openDemoLesson,
-} from '../../../../support/student-auth'
+import { openDemoLesson } from '../../../../support/student-auth'
 
 describe('Lesson next steps - Next lesson', () => {
     it('continues to the next lesson', () => {
         openDemoLesson()
         cy.scrollTo('bottom')
         cy.location('href').then(startUrl => {
-            clickLessonNextStep(/Proceed to the next lesson|Next lesson/i)
+            cy.contains('button, a, [role="button"]', /^\s*Next\b/i, {
+                timeout: 30000,
+            }).filter(':visible').last().click({ force: true })
             cy.location('href', { timeout: 30000 }).should('not.eq', startUrl)
         })
         cy.get('body').should('not.contain.text', 'Default blank page')
