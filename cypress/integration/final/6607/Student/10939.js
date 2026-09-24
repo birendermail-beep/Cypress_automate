a0634c073d4280e9d90c59e8edbdf33f5cba27ef
birendermail-beep <birender.mail@gmail.com>
2026-09-24 16:01:12 +0530

    Use visible lesson next-step actions in 10939.js

--- cypress/integration/final/6607/Student/10939.js
+++ cypress/integration/final/6607/Student/10939.js
@@ -1,13 +1,14 @@
 /* @story_id: 10939 @story_name: Open Quiz */
-import { openDemoLesson } from '../../../../support/student-auth'
+import {
+    clickLessonNextStep,
+    openDemoLesson,
+} from '../../../../support/student-auth'
 
 describe('Lesson next steps - Quiz', () => {
     it('opens the chapter quiz without a blank page', () => {
         openDemoLesson()
         cy.scrollTo('bottom')
-        cy.get('[data-cy="quiz_open"]', { timeout: 30000 })
-            .filter(':visible').first().scrollIntoView()
-            .invoke('removeAttr', 'target').click({ force: true })
+        clickLessonNextStep(/Quiz|Test your knowledge/i)
         cy.get('body', { timeout: 30000 })
             .should('be.visible')
             .and('not.contain.text', 'Default blank page')
