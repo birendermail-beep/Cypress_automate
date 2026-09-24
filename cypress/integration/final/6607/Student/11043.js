e92adfd6fa865f6423f000019089a4a5a98ae545
birendermail-beep <birender.mail@gmail.com>
2026-09-24 21:01:39 +0530

    Fix label selector in 11043.js

--- cypress/integration/final/6607/Student/11043.js
+++ cypress/integration/final/6607/Student/11043.js
@@ -4,7 +4,7 @@ import { visitDemoCourse } from '../../../../support/student-auth'
 describe('Post Assessment', () => {
     it('opens Post Assessment from the course dashboard', () => {
         visitDemoCourse()
-        cy.contains(':visible', /^\\s*Post Assessment\\s*$/i, {
+        cy.contains(':visible', /^\s*Post Assessment\s*$/i, {
             timeout: 30000,
         }).last().scrollIntoView().click({ force: true })
         cy.get('body', { timeout: 30000 })
