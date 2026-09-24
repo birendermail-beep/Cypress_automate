a092a3d31a89559457e940a4e331e339d3319bac
birendermail-beep <birender.mail@gmail.com>
2026-09-24 20:16:36 +0530

    Use stable next lesson toolbar interaction

--- cypress/integration/final/6607/Student/10942.js
+++ cypress/integration/final/6607/Student/10942.js
@@ -1,16 +1,16 @@
 /* @story_id: 10942 @story_name: Go to next chapter */
-import { openDemoLesson } from '../../../../support/student-auth'
+import {
+    openDemoLesson,
+    openLessonToolbarActivity,
+} from '../../../../support/student-auth'
 
 describe('Lesson next steps - Next lesson', () => {
     it('continues to the next lesson', () => {
         openDemoLesson()
         cy.scrollTo('bottom')
-        cy.location('href').then(startUrl => {
-            cy.contains('button, a, [role="button"]', /^\s*Next\b/i, {
-                timeout: 30000,
-            }).filter(':visible').last().click({ force: true })
-            cy.location('href', { timeout: 30000 }).should('not.eq', startUrl)
-        })
-        cy.get('body').should('not.contain.text', 'Default blank page')
+        openLessonToolbarActivity(/^\s*Next\b/i)
+        cy.get('body', { timeout: 30000 })
+            .should('be.visible')
+            .and('not.contain.text', 'Default blank page')
     })
 })
