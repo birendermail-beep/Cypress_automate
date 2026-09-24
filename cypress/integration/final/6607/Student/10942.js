775f70082762f1832598ebe16a8d029f9087c6ff
birendermail-beep <birender.mail@gmail.com>
2026-09-24 14:42:55 +0530

    Fix lesson navigation in 10942.js

--- cypress/integration/final/6607/Student/10942.js
+++ cypress/integration/final/6607/Student/10942.js
@@ -1,10 +1,10 @@
 /* @story_id: 10942 @story_name: Go to next chapter */
-import { visitDemoCourse } from '../../../../support/student-auth'
+import { openDemoLesson } from '../../../../support/student-auth'
 
 describe('Lesson next steps - Next lesson', () => {
     it('continues to the next lesson', () => {
-        visitDemoCourse()
-        cy.visit('/app/?func=ebook&chapter_no=0')
+        openDemoLesson()
+        cy.scrollTo('bottom')
         cy.location('href').then(startUrl => {
             cy.get('[data-cy="next_steps_open"]', { timeout: 30000 })
                 .filter(':visible').first().scrollIntoView().click({ force: true })
