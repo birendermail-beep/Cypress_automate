df8afa3c2baa980b5caf189602cd3085fbe29ddc
birendermail-beep <birender.mail@gmail.com>
2026-09-24 16:32:43 +0530

    Target exact lesson toolbar control in 10939.js

--- cypress/integration/final/6607/Student/10939.js
+++ cypress/integration/final/6607/Student/10939.js
@@ -1,14 +1,15 @@
 /* @story_id: 10939 @story_name: Open Quiz */
-import {
-    clickLessonNextStep,
-    openDemoLesson,
-} from '../../../../support/student-auth'
+import { openDemoLesson } from '../../../../support/student-auth'
 
 describe('Lesson next steps - Quiz', () => {
     it('opens the chapter quiz without a blank page', () => {
         openDemoLesson()
         cy.scrollTo('bottom')
-        clickLessonNextStep(/Quiz|Test your knowledge/i)
+        cy.contains('button, a, [role="button"]', /^\s*Quiz\s*$/i, {
+            timeout: 30000,
+        }).filter(':visible').last()
+            .invoke('removeAttr', 'target')
+            .click({ force: true })
         cy.get('body', { timeout: 30000 })
             .should('be.visible')
             .and('not.contain.text', 'Default blank page')
