c00f6d49895c4fe7e185190322e6a31b99ec2737
birendermail-beep <birender.mail@gmail.com>
2026-09-24 14:41:44 +0530

    Fix lesson navigation in 10929.js

--- cypress/integration/final/6607/Student/10929.js
+++ cypress/integration/final/6607/Student/10929.js
@@ -1,13 +1,9 @@
 /* @story_id: 10929 @story_name: Manage Setting - Options */
-import { visitDemoCourse } from '../../../../support/student-auth'
+import { openDemoLesson } from '../../../../support/student-auth'
 
 describe('Manage settings options in the lesson area', () => {
     it('shows the lesson settings options', () => {
-        visitDemoCourse()
-        cy.visit('/app/?func=ebook&chapter_no=0')
-        cy.get('body', { timeout: 30000 })
-            .should('be.visible')
-            .and('not.contain.text', 'Default blank page')
+        openDemoLesson()
         cy.get('#manage_settg', { timeout: 30000 })
             .filter(':visible').first().click({ force: true })
         cy.get('#fcs', { timeout: 30000 }).should('be.visible')
