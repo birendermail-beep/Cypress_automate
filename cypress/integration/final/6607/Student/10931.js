adeddc7875927f55ede60101ff2183f288c28089
birendermail-beep <birender.mail@gmail.com>
2026-09-24 14:42:05 +0530

    Fix lesson navigation in 10931.js

--- cypress/integration/final/6607/Student/10931.js
+++ cypress/integration/final/6607/Student/10931.js
@@ -1,10 +1,9 @@
 /* @story_id: 10931 @story_name: Keyboard Shortcut */
-import { visitDemoCourse } from '../../../../support/student-auth'
+import { openDemoLesson } from '../../../../support/student-auth'
 
 describe('Keyboard shortcuts in the lesson area', () => {
     it('opens the keyboard-shortcut list', () => {
-        visitDemoCourse()
-        cy.visit('/app/?func=ebook&chapter_no=0')
+        openDemoLesson()
         cy.get('#manage_settg', { timeout: 30000 })
             .filter(':visible').first().click({ force: true })
         cy.get('#kbd', { timeout: 30000 })
