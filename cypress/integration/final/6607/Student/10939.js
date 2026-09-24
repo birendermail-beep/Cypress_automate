b6672df77024741cbc87187fcbacf2b5114223ce
birendermail-beep <birender.mail@gmail.com>
2026-09-24 16:37:26 +0530

    Handle lesson activity popup in 10939.js

--- cypress/integration/final/6607/Student/10939.js
+++ cypress/integration/final/6607/Student/10939.js
@@ -1,15 +1,14 @@
 /* @story_id: 10939 @story_name: Open Quiz */
-import { openDemoLesson } from '../../../../support/student-auth'
+import {
+    openDemoLesson,
+    openLessonToolbarActivity,
+} from '../../../../support/student-auth'
 
 describe('Lesson next steps - Quiz', () => {
     it('opens the chapter quiz without a blank page', () => {
         openDemoLesson()
         cy.scrollTo('bottom')
-        cy.contains('button, a, [role="button"]', /^\s*Quiz\s*$/i, {
-            timeout: 30000,
-        }).filter(':visible').last()
-            .invoke('removeAttr', 'target')
-            .click({ force: true })
+        openLessonToolbarActivity(/^\s*Quiz\s*$/i)
         cy.get('body', { timeout: 30000 })
             .should('be.visible')
             .and('not.contain.text', 'Default blank page')
