d00f55cca839f9d8de2bb7dc27efc15edec0dd18
birendermail-beep <birender.mail@gmail.com>
2026-09-26 14:39:44 +0530

    Modernize Student 21561 test

--- cypress/integration/final/6607/Student/21561.js
+++ cypress/integration/final/6607/Student/21561.js
@@ -1,42 +1,14 @@
+/* @story_id: 21561 @story_name: Sign In */
+import { restoreStudentLogin } from '../../../../support/student-auth'
 
-/*
-@author: Shashank Gupta
-@master_project_id: 7761
-@phase_id: 11631
-@story_id: 21561
-@story_name: Sign In
-@path: 6607/Student/21561.js
-@test_case_name: Sign In or Sign Up
-@description: Sign In
-@test_steps:
-^Use unregistered email
-- Go to course dashboard
-- Click on the any course component
-- A login modal box willbe open
-- Fill unregistered email and password
-- Click the Sign button
-
-^Use correct email and wrong password
-- Go to course dashboard
-- Click on the any course component
-- A login modal box willbe open
-- Fill wrong email and password
-- Click the Sign button
-
-^Use wrong email and correct password
-- Go to course dashboard
-- Click on the any course component
-- A login modal box willbe open
-- Fill wrong email and password
-- Click the Sign button
-
-^Use correct email and correct password
-- Go to course dashboard
-- Click on the any course component
-- A login modal box willbe open
-- Fill wrong email and password
-- Click the Sign button
-
-@test_data: N/A
-@result: If credentials are correct, the user will be logged in. Otherwise, the user will get an error message.
-*/
+describe('Student sign in', () => {
+    it('signs in with the configured student account', () => {
+        restoreStudentLogin()
+        cy.visit('/app/')
+        cy.location('pathname', { timeout: 30000 })
+            .should('not.include', 'login.php')
+        cy.get('body').should('be.visible')
+            .and('not.contain.text', 'Default blank page')
+        cy.get('body').should('not.contain.text', 'ENTER PASSWORD')
+    })
+})
