d23504288afdb742a43b3352189397398bf7cb08
birendermail-beep <birender.mail@gmail.com>
2026-09-26 13:33:38 +0530

    Modernize Student 15447 with cached login

--- cypress/integration/final/6607/Student/15447.js
+++ cypress/integration/final/6607/Student/15447.js
@@ -1,33 +1,11 @@
-/*
-@author: Sundaram Tripathi
-@master_project_id: 6607
-@phase_id: 
-@Story_Id: 15447
-@story_name: amazon_login
-@path: final/6607/Student
-@Test_Case_Name: amazon_login 
-@description: Login the open amazon page
-@test_steps: 
+/* @story_id: 15447 @story_name: Amazon login */
+import { restoreStudentLogin } from '../../../../support/student-auth'
 
-^Test case of Edit option in product tab
--visit on website
-- Visit on this click "https://www.jigyaasa.info/login.php?func=amazon_login"
-
-@test_data: Search = testbot@ucertify.com
-@result: Successfully open amazon page
-*/
-import { login_username, login_password } from '../../../../../config'
-import Navbar from '../../../../page-objects/components/Navbar'
-import LoginPage from '../../../../page-objects/pages/LoginPage'
-describe('Amazone Page', function() {
-    it('Visit the amazone page', function() {
-        cy.fixture('global').then(data => {
-            cy.visit(data.url)
-            Navbar.clickOnLogin()
-            LoginPage.loginPage(login_username, login_password)
-            cy.visit(data.url);
-            cy.get(':nth-child(2) > .list-unstyled > :nth-child(1) > a > .pointer').click({ force: true });
-            cy.visit(data.url + '/login.php?func=amazon_login');
-        })
+describe('Amazon login route', () => {
+    it('opens the Amazon login route without a blank page', () => {
+        restoreStudentLogin()
+        cy.visit('/login.php?func=amazon_login', { failOnStatusCode: false })
+        cy.location('href', { timeout: 30000 }).should('not.eq', 'about:blank')
+        cy.get('body').should('be.visible')
     })
 })
