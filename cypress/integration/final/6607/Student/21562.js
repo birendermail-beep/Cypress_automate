4602edbf1173889ed3880cb4f016f8c65d2b60e0
birendermail-beep <birender.mail@gmail.com>
2026-09-26 14:40:02 +0530

    Modernize Student 21562 test

--- cypress/integration/final/6607/Student/21562.js
+++ cypress/integration/final/6607/Student/21562.js
@@ -1,32 +1,28 @@
+/* @story_id: 21562 @story_name: Sign Up */
 
-/*
-@author: Shashank Gupta
-@master_project_id: 7761
-@phase_id: 11631
-@story_id: 21562
-@story_name: Sign Up
-@path: 6607/Student/21562.js
-@test_case_name: Sign Up
-@description: Sign up
-@test_steps:
-^Use registered email
-- Go to course dashboard
-- Click on the any course component
-- A login modal box willbe open
-- Click the Sign Up link
-- Sign Up inputs will be shown
-- Fill a alreday registered email
-- Click the Sign Up button
+describe('Student sign up', () => {
+    it('opens the Sign Up form without creating an account', () => {
+        cy.visit('/login.php', { failOnStatusCode: false })
+        cy.get('body', { timeout: 30000 }).should('be.visible')
 
-^Use unregistered email
-- Go to course dashboard
-- Click on the any course component
-- A login modal box willbe open
-- Click the Sign Up link
-- Sign Up inputs will be shown
-- Fill a alreday unregistered email
-- Click the Sign Up button
+        cy.contains('a, button', /^\s*Sign\s*Up\s*$/i, { timeout: 30000 })
+            .filter(':visible')
+            .first()
+            .then($action => {
+                const href = $action.attr('href')
+                if (href && !href.startsWith('javascript:')) {
+                    cy.visit(href, { failOnStatusCode: false })
+                } else {
+                    cy.wrap($action).click({ force: true })
+                }
+            })
 
-@test_data: N/A
-@result: If the user email is correct, the user will be registered with uCertify and get the Activation Code via activation email. Otherwise, the user will get an error message.
-*/
+        cy.get('body', { timeout: 30000 }).should('be.visible')
+            .and('not.contain.text', 'Default blank page')
+        cy.get(
+            'input[type="email"], input[name*="email"], input[placeholder*="EMAIL"]',
+            { timeout: 30000 }
+        ).filter(':visible').should('have.length.greaterThan', 0)
+        // Intentionally do not submit: the test must not create a real account.
+    })
+})
